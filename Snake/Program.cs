7465d879932f77c4352831375cb4f25d403807a6
madflea <madflea@mail.ru>
2022-05-11 01:17:14 +0400

    Создали функцию движения змейки

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Snake
 {
@@ -23,6 +24,25 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+
         }
     }
 }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Snake
 {
     public class Snake : Figure
     {
-        public Snake(Point tail, int length, Direction direction)
+        Direction direction;
+
+        public Snake(Point tail, int length, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for(int i = 0; i < length; i++)
             {
@@ -14,6 +18,25 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
